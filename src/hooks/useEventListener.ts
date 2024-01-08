a0c94f6e8869d467440d76ed0ff78f119daed49a
黄铭坤 <1198966495@qq.com>
2024-01-08 21:40:45 +0800

    代码优化调整

--- src/hooks/useEventListener.ts
+++ src/hooks/useEventListener.ts
@@ -2,13 +2,12 @@ import { onMounted, onUnmounted } from "vue";
 export function useEventListener(
   el: Window | Document | HTMLElement,
   event: string,
-  handler: (event: Event) => void
+  callback: (event: Event) => void
 ) {
-  const savedHandler = handler;
   onMounted(() => {
-    el.addEventListener(event, savedHandler);
+    el.addEventListener(event, callback);
   });
   onUnmounted(() => {
-    el.removeEventListener(event, savedHandler);
+    el.removeEventListener(event, callback);
   });
 }
